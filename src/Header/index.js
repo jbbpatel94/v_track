db434a0c3f2f4ac16696a20417b7a8cb8ff47458
LalitKishor <lalitsinghbanna05@gmail.com>
2018-09-16 18:04:09 +0530

    improve herader perfomance

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,56 +1,298 @@
-
-import React, { Component } from 'react';
-import Grid from '@material-ui/core/Grid';
-
-import Paper from '@material-ui/core/Paper';
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import Input from '@material-ui/core/Input';
+import Badge from '@material-ui/core/Badge';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import { fade } from '@material-ui/core/styles/colorManipulator';
 import { withStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MailIcon from '@material-ui/icons/Mail';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import MoreIcon from '@material-ui/icons/MoreVert';
 import { Link, NavLink } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
-
 const styles = theme => ({
-    header: {
-        alignItems: "center",
-        display: "flex",
-        justifyContent: "space-around"
+  root: {
+    width: '100%',
+  },
+  home:{
+      textDecoration:"none",
+      color:"white"
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
     },
-    paper: {
-        width: "100",
-        padding: theme.spacing.unit * 2,
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
     },
+    marginRight: theme.spacing.unit * 2,
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing.unit * 3,
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 9,
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%',
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: 200,
+    },
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
 });
 
+class PrimarySearchAppBar extends React.Component {
+  state = {
+    anchorEl: null,
+    mobileMoreAnchorEl: null,
+  };
 
+  handleProfileMenuOpen = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
 
+  handleMenuClose = () => {
+    this.setState({ anchorEl: null });
+    this.handleMobileMenuClose();
+  };
 
+  handleMobileMenuOpen = event => {
+    this.setState({ mobileMoreAnchorEl: event.currentTarget });
+  };
 
-class Header extends Component {
-    render() {
-        const { classes } = this.props;
-        return (
-            <div>
-                <Grid container item xs={12} className={classes.header}>
-                    <Button  component={NavLink} to="/" exact  activeStyle={{
-                            fontWeight: 'bold',
-                            color: '#2196f3'
+  handleMobileMenuClose = () => {
+    this.setState({ mobileMoreAnchorEl: null });
+  };
+
+  render() {
+    const { anchorEl, mobileMoreAnchorEl } = this.state;
+    const { classes } = this.props;
+    const isMenuOpen = Boolean(anchorEl);
+    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+    const renderMenu = (
+      <Menu
+        anchorEl={anchorEl}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        open={isMenuOpen}
+        onClose={this.handleMenuClose}
+      >
+        <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+        <MenuItem onClick={this.handleClose}>
+            <NavLink to="/login" exact  activeStyle={{
+                                    fontWeight: 'bold',
+                                    color: '#2196f3',
+                                    textDecoration :"none"
+                                }}>
+                    LogIn
+            </NavLink>
+        </MenuItem>
+      </Menu>
+    );
+
+    const renderMobileMenu = (
+      <Menu
+        anchorEl={mobileMoreAnchorEl}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        open={isMobileMenuOpen}
+        onClose={this.handleMobileMenuClose}
+      >
+        {/* <MenuItem>
+          <IconButton color="inherit">
+            <Badge className={classes.margin} badgeContent={4} color="secondary">
+              <MailIcon />
+            </Badge>
+          </IconButton>
+          <p>Messages</p>
+        </MenuItem>
+        <MenuItem>
+          <IconButton color="inherit">
+            <Badge className={classes.margin} badgeContent={11} color="secondary">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+          <p>Notifications</p>
+        </MenuItem> */}
+        <MenuItem onClick={this.handleProfileMenuOpen}>
+          <IconButton color="inherit">
+            <AccountCircle />
+          </IconButton>
+          <p>Profile</p>
+        </MenuItem>
+      </Menu>
+    );
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
+              <MenuIcon />
+            </IconButton>
+            <Typography className={classes.title} variant="title" color="inherit" noWrap>
+            <NavLink to="/" className={classes.home} exact  activeStyle={{
+                            // fontWeight: 'bold',
+                            color: 'white',
+                            textDecoration :"none"
                         }}>
-                        Home
-                    </Button>
-                   
-                    
-                        V-track
-                   
-                    <Button component={NavLink} to="/login" exact  activeStyle={{
-                            fontWeight: 'bold',
-                            color: '#2196f3'
-                        }}>
-                        LogIn
-                    </Button>
-                </Grid>
+                        V-Track
+            </NavLink>
+            </Typography>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <Input
+                placeholder="Search…"
+                disableUnderline
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+              />
             </div>
-        );
-    }
+            <div className={classes.grow} />
+            <div className={classes.sectionDesktop}>
+              {/* <IconButton color="inherit">
+                <Badge className={classes.margin} badgeContent={4} color="secondary">
+                  <MailIcon />
+                </Badge>
+              </IconButton>
+              <IconButton color="inherit">
+                <Badge className={classes.margin} badgeContent={17} color="secondary">
+                  <NotificationsIcon />
+                </Badge>
+              </IconButton> */}
+              <IconButton
+                aria-owns={isMenuOpen ? 'material-appbar' : null}
+                aria-haspopup="true"
+                onClick={this.handleProfileMenuOpen}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+            </div>
+            <div className={classes.sectionMobile}>
+              <IconButton aria-haspopup="true" onClick={this.handleMobileMenuOpen} color="inherit">
+                <MoreIcon />
+              </IconButton>
+            </div>
+          </Toolbar>
+        </AppBar>
+        {renderMenu}
+        {renderMobileMenu}
+      </div>
+    );
+  }
 }
 
-export default withStyles(styles, { withTheme: true })(Header);
+PrimarySearchAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(PrimarySearchAppBar);
+// import Paper from '@material-ui/core/Paper';
+// import { withStyles } from '@material-ui/core/styles';
+// import { Link, NavLink } from 'react-router-dom';
+// import Button from '@material-ui/core/Button';
+
+// const styles = theme => ({
+//     header: {
+//         alignItems: "center",
+//         display: "flex",
+//         justifyContent: "space-around"
+//     },
+//     paper: {
+//         width: "100",
+//         padding: theme.spacing.unit * 2,
+//         textAlign: 'center',
+//         color: theme.palette.text.secondary,
+//     },
+// });
+
+
+
+
+
+// class Header extends Component {
+//     render() {
+//         const { classes } = this.props;
+//         return (
+//             <div>
+//                 <Grid container item xs={12} className={classes.header}>
+//                     <Button  component={NavLink} to="/" exact  activeStyle={{
+//                             fontWeight: 'bold',
+//                             color: '#2196f3'
+//                         }}>
+//                         Home
+//                     </Button>
+                   
+                    
+//                         V-track
+                   
+//                     <Button component={NavLink} to="/login" exact  activeStyle={{
+//                             fontWeight: 'bold',
+//                             color: '#2196f3'
+//                         }}>
+//                         LogIn
+//                     </Button>
+//                 </Grid>
+//             </div>
+//         );
+//     }
+// }
+
+// export default withStyles(styles, { withTheme: true })(Header);
